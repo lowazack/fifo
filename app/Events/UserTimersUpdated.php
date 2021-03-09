7196b9a9864a93a690c9585c383f4373091a9b9d
Jack <loaizajack@gmail.com>
2021-03-09 10:22:50 +0000

    emmitting event when creating new timer

---
+++ app/Events/UserTimersUpdated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserTimersUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * @return Channel
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel("user-timers");
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'Timers-Updated';
+    }
+}
---
+++ app/Observers/TimerObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Events\TeamTimersUpdated;
+use App\Events\UserTimersUpdated;
+use App\Models\TimeEntry;
+
+class TimerObserver
+{
+    /**
+     * Handle the TimeEntry "created" event.
+     *
+     * @param TimeEntry $timeEntry
+     * @return void
+     */
+    public function created(TimeEntry $timeEntry)
+    {
+//        event(new TeamTimersUpdated());
+        event(new UserTimersUpdated());
+    }
+
+    /**
+     * @param TimeEntry $timeEntry
+     */
+    public function updated(TimeEntry $timeEntry)
+    {
+
+    }
+
+    /**
+     * @param TimeEntry $timeEntry
+     */
+    public function deleted(TimeEntry $timeEntry)
+    {
+        //
+    }
+
+    /**
+     * @param TimeEntry $timeEntry
+     */
+    public function restored(TimeEntry $timeEntry)
+    {
+        //
+    }
+
+    /**
+     * @param TimeEntry $timeEntry
+     */
+    public function forceDeleted(TimeEntry $timeEntry)
+    {
+        //
+    }
+}
